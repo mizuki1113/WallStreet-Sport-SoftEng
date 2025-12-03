6102668270f6a979363492e36fa74eedcd2c7c20
mizuki <pgamalo56@gmail.com>
2025-12-03 10:15:09 +0800

    load logo

--- WallStreet Backend/src/services/EmailService.ts
+++ WallStreet Backend/src/services/EmailService.ts
@@ -135,7 +135,12 @@ export class EmailService {
 <body>
   <div class="email-container">
     <div class="logo">
-      <img src="https://i.imgur.com/nAbwdb0.png" alt="WallStreet Sport Logo">
+      <img 
+  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAABdwAAAH0CAIAAACo53h7AACAAElEQVR4nGz9Z3s...sqjoC+lBdLO2hDSFaaP9h53p3R6cJ6uKF18pEeJxyf3q2l/4FICHpMxWvYFnqkx" 
+  alt="WallStreet Sport Logo"
+  style="max-width:600px;height:auto;"
+/>
+
     </div>
     
     <div class="content">
